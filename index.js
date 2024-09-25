d843eb5b8f89acf4b41c089457a98bc5af716b0d
Jordan Patton <jordanpatton@gmail.com>
2024-09-25 17:45:17 -0400

    use default page in headful mode

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import puppeteer from 'puppeteer';
 import { sleep } from './helpers.js';
 
 const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage();
+const [page] = await browser.pages();
 await page.goto('https://accounts.shutterfly.com');
 await page.setViewport({ height: 768, width: 1024 });
 await sleep(5000);
 
 const emailInputHandle = await page.locator('input#email').waitHandle();
-const emailInputText = await emailInputHandle?.evaluate(element => element.textContent);
-console.log('emailInputText:', emailInputText);
-
-const signInButtonHandle = await page.locator('button#signInButton.submit').waitHandle();
-const signInButtonText = await signInButtonHandle?.evaluate(element => element.textContent);
-console.log('signInButtonText:', signInButtonText);
+const emailInputValue = await emailInputHandle?.evaluate(element => element.value);
+const passwordInputHandle = await page.locator('input#password').waitHandle();
+const passwordInputValue = await passwordInputHandle?.evaluate(element => element.value);
+if (emailInputValue.length && passwordInputValue.length) {
+    console.log(`SUCCESS: Logged in with ${emailInputValue}:${passwordInputValue}`);
+} else {
+    console.log('FAILURE: Failed to log in.');
+}
 
 await browser.close();
