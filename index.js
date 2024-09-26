f6e50a9ab22a1920699ee900429e8a9192be4427
Jordan Patton <jordanpatton@gmail.com>
2024-09-25 20:08:10 -0400

    weird async polling

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ import puppeteer from 'puppeteer';
 
 import { sleep } from './helpers.js';
 
-let testTimeout;
-function test() {
-    console.log(Date.now());
-    testTimeout = setTimeout(test, 1000);
-}
-test();
+let pageWasPrematurelyClosed = true;
+let shouldContinuePollingForInput = true;
 
 const browser = await puppeteer.launch({ headless: false });
 const [page] = await browser.pages(); // use default page
-let pageWasPrematurelyClosed = true;
 page.once('close', () => {
-    clearTimeout(testTimeout);
+    shouldContinuePollingForInput = false;
     // When the page is closed before the puppeteer script is complete, the node process
     // will hang. In that case we must manually terminate the node process. For some
     // unknown reason this dumps an error to stdout unless we first `browser.close()`.
@@ -25,16 +20,18 @@ page.once('close', () => {
 
 await page.goto('https://accounts.shutterfly.com');
 await page.setViewport({ height: 768, width: 1024 });
-await sleep(5000);
 
 const emailInputHandle = await page.locator('input#email').waitHandle();
-const emailInputValue = await emailInputHandle?.evaluate(element => element.value);
 const passwordInputHandle = await page.locator('input#password').waitHandle();
-const passwordInputValue = await passwordInputHandle?.evaluate(element => element.value);
-if (emailInputValue.length && passwordInputValue.length) {
-    console.log(`SUCCESS: Logged in with ${emailInputValue}:${passwordInputValue}`);
-} else {
-    console.log('FAILURE: Failed to log in.');
+
+while(shouldContinuePollingForInput) {
+    const emailInputValue = await emailInputHandle?.evaluate(element => element.value);
+    const passwordInputValue = await passwordInputHandle?.evaluate(element => element.value);
+    if (emailInputValue.length && passwordInputValue.length) {
+        shouldContinuePollingForInput = false;
+        console.log(`SUCCESS: Logged in with ${emailInputValue}:${passwordInputValue}.`);
+    }
+    await sleep(1000);
 }
 
 pageWasPrematurelyClosed = false;
