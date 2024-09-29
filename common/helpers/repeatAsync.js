51d8e43f223aa4856d2ec6a24462785e14cc4bca
Jordan Patton <jordanpatton@gmail.com>
2024-09-29 15:29:11 -0400

    renamed taskFunction to task

--- common/helpers/repeatAsync.js
+++ common/helpers/repeatAsync.js
@@ -1,20 +1,20 @@
 import { sleepAsync } from './sleepAsync.js';
 
-/** Repetition stops when this signal is returned by `taskFunction`. */
+/** Repetition stops when this signal is returned by `task`. */
 export const STOP_SIGNAL = 'REPEAT_ASYNC_STOP_SIGNAL';
 
 /**
- * Recursively invokes user-defined `taskFunction` until stopped. Repetition stops when
- * `taskFunction` returns `STOP_SIGNAL`.
- * @param {function} taskFunction User-defined task to be repeated. Must return `STOP_SIGNAL` when repetition should stop.
+ * Recursively invokes user-defined `task` until stopped. Repetition stops when `task`
+ * returns `STOP_SIGNAL`.
+ * @param {function} task User-defined behavior to be repeated. Must return `STOP_SIGNAL` when repetition should stop.
  * @param {number} sleepMilliseconds How long to sleep (in milliseconds) between repetitions.
  * @param {function} callerResolve `resolve` function from caller `Promise`.
  * @param {function} callerReject `reject` function from caller `Promise`.
  * @returns {void}
  */
-const repeatAsyncHelper = (taskFunction, sleepMilliseconds, callerResolve, callerReject) => {
-    if (typeof taskFunction !== 'function') {
-        throw new TypeError('taskFunction must have type: function.');
+const repeatAsyncHelper = (task, sleepMilliseconds, callerResolve, callerReject) => {
+    if (typeof task !== 'function') {
+        throw new TypeError('task must have type: function.');
     }
     if (typeof sleepMilliseconds !== 'number') {
         throw new TypeError('sleepMilliseconds must have type: number.');
@@ -25,14 +25,14 @@ const repeatAsyncHelper = (taskFunction, sleepMilliseconds, callerResolve, calle
     if (typeof callerReject !== 'function') {
         throw new TypeError('callerReject must have type: function.');
     }
-    const result = taskFunction(STOP_SIGNAL);
+    const result = task(STOP_SIGNAL);
     (result instanceof Promise ? result : Promise.resolve(result)).then(
         (signal) => {
             if (signal === STOP_SIGNAL) {
                 callerResolve(); // do not recurse
             } else {
                 sleepAsync(sleepMilliseconds).then(
-                    () => {repeatAsyncHelper(taskFunction, sleepMilliseconds, callerResolve, callerReject);}, // recurse
+                    () => {repeatAsyncHelper(task, sleepMilliseconds, callerResolve, callerReject);}, // recurse
                     () => {callerReject('ERROR: Sleep failed.');},
                 );
             }
@@ -42,20 +42,20 @@ const repeatAsyncHelper = (taskFunction, sleepMilliseconds, callerResolve, calle
 };
 
 /**
- * Repeats user-defined `taskFunction` until stopped. Repetition stops when `taskFunction`
- * returns `STOP_SIGNAL`. `async`-compatible.
- * @param {function} taskFunction User-defined task to be repeated. Must return `STOP_SIGNAL` when repetition should stop.
+ * Repeats user-defined `task` until stopped. Repetition stops when `task` returns
+ * `STOP_SIGNAL`. `async`-compatible.
+ * @param {function} task User-defined behavior to be repeated. Must return `STOP_SIGNAL` when repetition should stop.
  * @param {number} [sleepMilliseconds=1000] How long to sleep (in milliseconds) between repetitions.
  * @returns {Promise} Resolves or rejects when repetition stops.
  */
-export const repeatAsync = (taskFunction, sleepMilliseconds = 1000) => {
-    if (typeof taskFunction !== 'function') {
-        throw new TypeError('taskFunction must have type: function.');
+export const repeatAsync = (task, sleepMilliseconds = 1000) => {
+    if (typeof task !== 'function') {
+        throw new TypeError('task must have type: function.');
     }
     if (typeof sleepMilliseconds !== 'number') {
         throw new TypeError('sleepMilliseconds must have type: number.');
     }
     return new Promise((resolve, reject) => {
-        repeatAsyncHelper(taskFunction, sleepMilliseconds, resolve, reject);
+        repeatAsyncHelper(task, sleepMilliseconds, resolve, reject);
     });
 };
