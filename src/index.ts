50b05e7fbb657f3c181e21b4049973733b421dfb
Jordan Patton <jordanpatton@gmail.com>
2024-10-03 02:40:22 -0400

    added date calculations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { ONE_DAY_IN_MILLISECONDS } from './common/constants.js';
 import { fetchPaginatedMomentsViaApi } from './fetchPaginatedMomentsViaApi.js';
 import { fetchSkeletonViaApi } from './fetchSkeletonViaApi.js';
 import { logInToShutterflyViaPuppeteer } from './logInToShutterflyViaPuppeteer.js';
@@ -17,8 +18,15 @@ const downloadPhotosFromShutterfly = async (): Promise<void> => {
         console.log('ERROR: Failed to fetch skeleton.');
         return;
     }
-    // TODO: sort skeleton by date, extract min/max, add/subtract 1 day, convert to seconds, pass to fetchPaginatedMomentsViaApi
-    console.log(skeleton);
+    skeleton.sort((a, b) => (new Date(a.date)).getTime() - (new Date(b.date)).getTime());
+    const skeletonEarliest = skeleton[0];
+    const startTimeUnixSeconds = Math.max(Math.round(((new Date(skeletonEarliest.date)).getTime() - ONE_DAY_IN_MILLISECONDS) / 1000), 0);
+    console.log(`Earliest moment: ${skeletonEarliest.date}.`);
+    console.log(`Start time: ${startTimeUnixSeconds} seconds since Unix epoch. (1 day before earliest moment.)`);
+    const skeletonLatest = skeleton[skeleton.length - 1];
+    const endTimeUnixSeconds = Math.round(((new Date(skeletonLatest.date)).getTime() + ONE_DAY_IN_MILLISECONDS) / 1000);
+    console.log(`Latest moment: ${skeletonLatest.date}.`);
+    console.log(`End time: ${endTimeUnixSeconds} seconds since Unix epoch. (1 day after latest moment.)`);
     // const moments = await fetchPaginatedMomentsViaApi(cognitoIdToken);
     // console.log(moments);
     return;
