caa0646677bb22998c601faedfaf88079498616d
Jordan Patton <jordanpatton@gmail.com>
2024-10-05 01:20:12 -0400

    moved getCommandLineParameter

---
+++ src/common/helpers/getCommandLineParameter.ts
@@ -0,0 +1,19 @@
+/**
+ * Parses the command line for a given parameter and returns its value.
+ * @param key - Key for a command line parameter.
+ * @returns Whether or not the `key` is present and the value associated with that `key`.
+ */
+export const getCommandLineParameter = (key: string) : {
+    keyIsPresent: boolean;
+    value: string | undefined;
+} => {
+    // Item 0 is the path to node executable.
+    // Item 1 is the path to the entry-point script.
+    // Any further items are optional parameters.
+    const argvSlice = process.argv.slice(2);
+    const keyIndex = argvSlice.indexOf(key);
+    return {
+        keyIsPresent: keyIndex !== -1,
+        value: (keyIndex !== -1 && keyIndex + 1 < argvSlice.length) ? argvSlice[keyIndex + 1] : undefined,
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { getCommandLineParameter } from './common/helpers/getCommandLineParameter.js';
 import { fetchMoments } from './fetchMoments.js';
 import { fetchSkeleton } from './fetchSkeleton.js';
 import { logInToShutterflyViaPuppeteer } from './logInToShutterflyViaPuppeteer.js';
@@ -13,22 +14,23 @@ const downloadPhotosFromShutterfly = async ({
     /** Identification token from Amazon Cognito authentication service. */
     cognitoIdToken?: string;
 }): Promise<void> => {
+    console.log('\nAuthenticating...');
+    console.group();
     let cognitoIdToken: string;
     if (typeof givenCognitoIdToken === 'string' && givenCognitoIdToken.length) {
         console.log(`Using Cognito idToken from command line:\n${givenCognitoIdToken}`);
         cognitoIdToken = givenCognitoIdToken;
     } else {
         console.log('Logging in to Shutterfly...');
-        console.group();
         const puppeteerCognitoIdToken = await logInToShutterflyViaPuppeteer();
         if (typeof puppeteerCognitoIdToken !== 'string' || !puppeteerCognitoIdToken.length) {
             throw new Error('ERROR: Failed to log in to Shutterfly.');
         }
-        console.groupEnd();
-        console.log('...done!');
-        console.log(`Using Cognito idToken from puppeteer:\n${puppeteerCognitoIdToken}`);
+        console.log(`Using Cognito idToken from Shutterfly:\n${puppeteerCognitoIdToken}`);
         cognitoIdToken = puppeteerCognitoIdToken;
     }
+    console.groupEnd();
+    console.log('...done!');
 
     console.log('\nDetermining time range...');
     console.group();
@@ -49,26 +51,6 @@ const downloadPhotosFromShutterfly = async ({
     console.log('...done!');
 
     return;
-};
-
-/**
- * Parses the command line for a given parameter and returns its value.
- * @param key - Key for a command line parameter.
- * @returns Whether or not the `key` is present and the value associated with that `key`.
- */
-const getCommandLineParameter = (key: string) : {
-    keyIsPresent: boolean;
-    value: string | undefined;
-} => {
-    // Item 0 is the path to node executable.
-    // Item 1 is the path to the entry-point script.
-    // Any further items are optional parameters.
-    const argvSlice = process.argv.slice(2);
-    const keyIndex = argvSlice.indexOf(key);
-    return {
-        keyIsPresent: keyIndex !== -1,
-        value: (keyIndex !== -1 && keyIndex + 1 < argvSlice.length) ? argvSlice[keyIndex + 1] : undefined,
-    };
 };
 
 downloadPhotosFromShutterfly({ cognitoIdToken: getCommandLineParameter('--cognitoIdToken').value });
