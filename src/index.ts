0631d4a6f8c12d3e9096378594754990c7a99217
Jordan Patton <jordanpatton@gmail.com>
2024-10-11 16:54:46 -0400

    add main entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import { getCommandLineParameter } from './common/helpers/getCommandLineParameter.js';
 import { downloadAssets, parseDownloadAssetsParameters } from './workflows/downloadAssets/index.js';
 
-downloadAssets(parseDownloadAssetsParameters());
+/**
+ * Main entrypoint for this program.
+ * 
+ * @returns Return value from workflow or void.
+ */
+export const main = (): ReturnType<typeof downloadAssets> | void => {
+    const workflow = getCommandLineParameter('--workflow').value;
+    if (typeof workflow === 'string') {
+        switch (workflow) {
+            case 'downloadAssets':
+                return downloadAssets(parseDownloadAssetsParameters());
+            default:
+                throw new RangeError('workflow (required) must be one of: downloadAssets.');
+        };
+    } else {
+        throw new TypeError('workflow (required) must be a non-empty string.');
+    }
+};
+
+main();
