45b7ad38f60563dce5ff74ef3b4fc0828983cc21
Jordan Patton <jordanpatton@gmail.com>
2024-10-18 00:44:25 -0400

    add comments

--- tsd/index.d.ts
+++ tsd/index.d.ts
@@ -1,2 +1,6 @@
-// This file declares items that should not be type-checked.
-// Example: declare module 'some-module';
+// This file declares modules that should not be type-checked. Example:
+// declare module 'module-without-type-checking';
+// It also declares modules with custom types and overrides. Example:
+// declare module 'module-with-custom-types' {
+//     export function doSomething(input: string): string;
+// }
