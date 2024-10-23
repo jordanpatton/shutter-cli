e43640f2a8c4d4d73997bace87b8c190497d1455
Jordan Patton <jordanpatton@gmail.com>
2024-10-23 17:01:15 -0400

    refactored authentication to be less confusing

--- src/components/authenticate/components/Authenticator/index.ts
+++ src/components/authenticate/components/Authenticator/index.ts
@@ -12,7 +12,51 @@ export class Authenticator {
     _session: ISession | undefined;
 
     /**
-     * Establishes an authenticated session, stores it in memory, and writes it to a file.
+     * Performs repetitive tasks for `authenticate`. (Validates and saves the in-memory `_session`.)
+     * 
+     * @param shouldWriteSessionToFile - Whether or not to write the in-memory session to file.
+     * @param isVerbose - Whether or not to be verbose.
+     * @returns Promisified boolean (`true` if all tasks were completed; otherwise `false`). Settles when done.
+     */
+    async _authenticateHelper(
+        shouldWriteSessionToFile: boolean = false,
+        sessionDescription: string = 'session',
+        isVerbose: boolean = false,
+    ): Promise<boolean> {
+        const consoleGroup = isVerbose ? console.group : () => {};
+        const consoleGroupEnd = isVerbose ? console.groupEnd : () => {};
+        const consoleLog = isVerbose ? console.log : () => {};
+
+        if (typeof this._session === 'undefined') {
+            return false;
+        }
+
+        consoleLog(`\nValidating ${sessionDescription}...`);
+        consoleGroup();
+        const sessionIsValid = validateSession(this._session, isVerbose);
+        consoleGroupEnd();
+        consoleLog('...done!');
+        if (!sessionIsValid) {
+            return false;
+        }
+
+        if (shouldWriteSessionToFile) {
+            consoleLog(`\nWriting ${sessionDescription} to file...`);
+            consoleGroup();
+            await writeStringToFileAsync({
+                fromString: JSON.stringify(this._session, null, 4),
+                toDirectory: SESSION_DIRECTORY,
+                toFileName: SESSION_FILE_NAME,
+            });
+            consoleGroupEnd();
+            consoleLog('...done!');
+        }
+
+        return true;
+    }
+
+    /**
+     * Establishes an authenticated session, stores it in memory, and writes it to file.
      * 
      * @param isVerbose - Whether or not to be verbose.
      * @returns Promisified Cognito idToken. Settles when data is ready.
@@ -22,100 +66,49 @@ export class Authenticator {
         const consoleGroupEnd = isVerbose ? console.groupEnd : () => {};
         const consoleLog = isVerbose ? console.log : () => {};
 
-        // .............................................................................................................
-        // 1. SHORT CIRCUIT: Check for an existing, valid session and return early. (No need to write it to file.)
-        // 1a. If in-memory session is undefined, then try to load it from file.
-        if (typeof this._session === 'undefined') {
-            consoleLog('\nLoading session from file...');
-            consoleGroup();
-            this._session = await readSessionFromFile(isVerbose);
-            consoleGroupEnd();
-            consoleLog('...done!');
+        // Check in-memory session.
+        if (await this._authenticateHelper(false, 'in-memory session', isVerbose)) {
+            return this.getCognitoIdToken()!; // guaranteed by `_authenticateHelper`
         }
-        // 1b. If in-memory session already exists or was loaded from file, then validate it and return early.
-        if (typeof this._session !== 'undefined') {
-            consoleLog('\nValidating existing session...');
-            consoleGroup();
-            if (validateSession(this._session, isVerbose)) {
-                consoleGroupEnd();
-                consoleLog('...done!');
-                // `validateSession` guarantees `idToken` exists and is a non-empty string.
-                return this._session.cognitoTokens.find(v => v.name.endsWith(COGNITO_TOKEN_NAME_POSTFIX_ID_TOKEN))!.value;
-            }
-            consoleGroupEnd();
-            consoleLog('...done!');
-        }
-        // .............................................................................................................
 
-        // .............................................................................................................
-        // 2. REFRESH EXISTING SESSION: Refresh an existing, invalid session. Validate it, write it to file, and return.
-        // 2a. If in-memory session already exists or was loaded from file, and it failed validation, then refresh it.
+        // If in-memory session does not exist or is invalid, then load from file.
+        consoleLog('\nLoading session from file...');
+        consoleGroup();
+        this._session = await readSessionFromFile(isVerbose);
+        consoleGroupEnd();
+        consoleLog('...done!');
+        if (await this._authenticateHelper(false, 'from-file session', isVerbose)) {
+            return this.getCognitoIdToken()!; // guaranteed by `_authenticateHelper`
+        }
+
+        // If in-memory session exists now due to the steps above (but it failed validation), then refresh it.
         if (typeof this._session !== 'undefined') {
             consoleLog('\nRefreshing existing session...');
             consoleGroup();
             this._session = await refreshSession(this._session, isVerbose);
             consoleGroupEnd();
             consoleLog('...done!');
-        }
-        // 2b. If session refreshed successfully, then validate it.
-        if (typeof this._session !== 'undefined') {
-            consoleLog('\nValidating refreshed session...');
-            consoleGroup();
-            if (validateSession(this._session, isVerbose)) {
-                consoleGroupEnd();
-                consoleLog('...done!');
-                // 2c. Write refreshed session to file and return.
-                consoleLog('\nWriting refreshed session to file...');
-                consoleGroup();
-                await writeStringToFileAsync({
-                    fromString: JSON.stringify(this._session, null, 4),
-                    toDirectory: SESSION_DIRECTORY,
-                    toFileName: SESSION_FILE_NAME,
-                });
-                consoleGroupEnd();
-                consoleLog('...done!');
-                // `validateSession` guarantees `idToken` exists and is a non-empty string.
-                return this._session.cognitoTokens.find(v => v.name.endsWith(COGNITO_TOKEN_NAME_POSTFIX_ID_TOKEN))!.value;
+            if (await this._authenticateHelper(true, 'refreshed session', isVerbose)) {
+                return this.getCognitoIdToken()!; // guaranteed by `_authenticateHelper`
             }
-            consoleGroupEnd();
-            consoleLog('...done!');
         }
-        // .............................................................................................................
 
-        // .............................................................................................................
-        // 3. CREATE NEW SESSION: Create a new session, validate it, write it to file, and return.
-        // 3a. If all else failed, then create a new session.
+        // If all else failed, then create a new session.
         consoleLog('\nCreating new session...');
         consoleGroup();
         this._session = await logIn();
         consoleGroupEnd();
         consoleLog('...done!');
-        // 3b. If new session was created successfully, then validate it.
-        if (typeof this._session !== 'undefined') {
-            consoleLog('\nValidating new session...');
-            consoleGroup();
-            if (validateSession(this._session, isVerbose)) {
-                consoleGroupEnd();
-                consoleLog('...done!');
-                // 3c. Write new session to file and return.
-                consoleLog('\nWriting new session to file...');
-                consoleGroup();
-                await writeStringToFileAsync({
-                    fromString: JSON.stringify(this._session, null, 4),
-                    toDirectory: SESSION_DIRECTORY,
-                    toFileName: SESSION_FILE_NAME,
-                });
-                consoleGroupEnd();
-                consoleLog('...done!');
-                // `validateSession` guarantees `idToken` exists and is a non-empty string.
-                return this._session.cognitoTokens.find(v => v.name.endsWith(COGNITO_TOKEN_NAME_POSTFIX_ID_TOKEN))!.value;
-            }
-            consoleGroupEnd();
-            consoleLog('...done!');
+        if (await this._authenticateHelper(true, 'new session', isVerbose)) {
+            return this.getCognitoIdToken()!; // guaranteed by `_authenticateHelper`
         }
-        // .............................................................................................................
 
         // There's nothing else we can do at this point.
         throw new Error('Failed to authenticate.');
+    }
+
+    /** Returns the Cognito idToken from the in-memory session. */
+    getCognitoIdToken(): string | undefined {
+        return this._session?.cognitoTokens.find(v => v.name.endsWith(COGNITO_TOKEN_NAME_POSTFIX_ID_TOKEN))?.value;
     }
 }
