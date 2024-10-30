10d86057200c5f07be1ff01b47ce5de210c9c780
Jordan Patton <jordanpatton@gmail.com>
2024-10-30 18:24:48 -0400

    converted tryUntilAsync options to object

--- src/utilities/tryUntilAsync.ts
+++ src/utilities/tryUntilAsync.ts
@@ -1,6 +1,23 @@
 import { promisifyByException } from './promisifyByException.js';
 import { sleepAsync } from './sleepAsync.js';
 
+interface ITryUntilAsyncOptions {
+    /** Whether or not to be verbose. */
+    isVerbose?: boolean;
+    /** How many times to try before giving up. Can be used simultaneously with `timeToLiveMilliseconds`. */
+    maximumNumberOfTries?: number;
+    /**
+     * How long to sleep (in milliseconds) between tries. By default, sleep time starts at 1000 milliseconds and grows
+     * exponentially according to the following formula: `1000 * Math.pow(2, recursionIndex)`.
+     */
+    sleepMilliseconds?: ((recursionIndex: number) => number) | number;
+    /**
+     * How long to continue trying (in milliseconds) before timing out. Can be used simultaneously with
+     * `maximumNumberOfTries`.
+     */
+    timeToLiveMilliseconds?: number;
+}
+
 /**
  * Recursively invokes user-defined `task` until one of the following occurs: `task` succeeds, this function times out,
  * or this function runs out of tries.
@@ -78,34 +95,32 @@ const tryUntilAsyncHelper = <TTaskResult>(
 
 /**
  * Repeatedly invokes user-defined `task` until one of the following occurs: `task` succeeds, this function times out,
- * or this function runs out of tries.
+ * or this function runs out of tries. `task` can be either synchronous or asynchronous, but must "fail" or "succeed"
+ * according to the following logic:
+ * - If `task` is synchronous, then it should "fail" by throwing an unhandled exception, and it should "succeed" by
+ *   completing without throwing an unhandled exception.
+ * - If `task` is asynchronous via manually returning a `Promise`, then it should "fail" by returning a rejected
+ *   `Promise`, and it should "succeed" by returning a resolved `Promise`.
+ * - If `task` is asynchronous via the `async` keyword, then it should "fail" by throwing an unhandled exception
+ *   (creating a rejected `Promise`), and it should "succeed" by completing without throwing an unhandled exception
+ *   (creating a resolved `Promise`).
  * 
- * @param task - User-defined behavior to be tried. Can be either synchronous or asynchronous, but must "fail" or
- *        "succeed" according to the following logic. (Note: If `task` cannot "fail", then this function will still
- *        work, but it will be completely pointless because `task` will always "succeed" on the first try.)
- *        - If `task` is synchronous, then it should "fail" by throwing an unhandled exception, and it should "succeed"
- *          by completing without throwing an unhandled exception.
- *        - If `task` is asynchronous via manually returning a `Promise`, then it should "fail" by returning a rejected
- *          `Promise`, and it should "succeed" by returning a resolved `Promise`.
- *        - If `task` is asynchronous via the `async` keyword, then it should "fail" by throwing an unhandled exception
- *          (creating a rejected `Promise`), and it should "succeed" by completing without throwing an unhandled
- *          exception (creating a resolved `Promise`).
- * @param sleepMilliseconds - How long to sleep (in milliseconds) between tries. By default, sleep time starts at 1000
- *        milliseconds and grows exponentially according to the following formula: `1000 * Math.pow(2, recursionIndex)`.
- * @param timeToLiveMilliseconds - How long to continue trying (in milliseconds) before timing out. Can be used
- *        simultaneously with `maximumNumberOfTries`.
- * @param maximumNumberOfTries - How many times to try before giving up. Can be used simultaneously with
- *        `timeToLiveMilliseconds`.
- * @param isVerbose - Whether or not to be verbose.
- * @returns Promisified task result. Settles when `task` succeeds, this function times out, or this function runs out of
- *          tries.
+ * If `task` cannot "fail", then this function will still work, but it will be completely pointless because `task` will
+ * always "succeed" on the first try.
+ * 
+ * @param task - User-defined behavior to be tried.
+ * @param options - Options.
+ * @returns Promisified `task` result. Settles when `task` succeeds, this function times out, or this function runs out
+ *          of tries.
  */
 export const tryUntilAsync = <TTaskResult>(
     task: () => Promise<TTaskResult> | TTaskResult,
-    sleepMilliseconds: ((recursionIndex: number) => number) | number = ((ri) => 1000 * Math.pow(2, ri)),
-    timeToLiveMilliseconds?: number,
-    maximumNumberOfTries?: number,
-    isVerbose: boolean = false,
+    {
+        isVerbose = false,
+        maximumNumberOfTries,
+        sleepMilliseconds = ((recursionIndex: number) => 1000 * Math.pow(2, recursionIndex)),
+        timeToLiveMilliseconds,
+    }: ITryUntilAsyncOptions = {},
 ): Promise<TTaskResult> =>
     new Promise((resolve, reject) => {
         tryUntilAsyncHelper(
